c929fae9852af096ac8e2163382d0d4679268045
Phirxian <phirxian@gmail.com>
2014-02-04 21:56:40 +0100

    Update 1meter.c
    c'est 120.;
    et %.2f pas %.2d

--- libs/comportment/1meter.c
+++ libs/comportment/1meter.c
@@ -1,11 +1,11 @@
 void main()
 {
     
-    const float distance = 120; /* The distance in centimeter */
+    const float distance = 120.; /* The distance in centimeter */
     
-    printf("running for\n%.2d meter", distance);
+    printf("running for\n%.2f cm", distance);
     init_motors();
     drive(100,0);
-    sleep(feetToMotor(feetToCm(distance), 100));
+    sleep(feetToMotor(cmToFeet(distance), 100));
     stop();
 }
