4cec52453b141648c6df2c37b66e03531479a88b
Guilhem117 <Theserialcracker117@gmail.com>
2014-02-04 18:00:24 +0100

    Update 1meter.c

--- libs/comportment/1meter.c
+++ libs/comportment/1meter.c
@@ -1,8 +1,11 @@
 void main()
 {
-    printf("runing for\n1.2 meter");
+    
+    const float distance = 120; /* The distance in centimeter */
+    
+    printf("running for\n%.2d meter", distance);
     init_motors();
     drive(100,0);
-    sleep(feetToMotor(feetToCm(120.), 100));
+    sleep(feetToMotor(feetToCm(distance), 100));
     stop();
 }
